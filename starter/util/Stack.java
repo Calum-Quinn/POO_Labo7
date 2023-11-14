d0f15538ddaf1cd3e20b9d3dd0430da9dacd49cd
dylanramos <dylan.oliveiraramos@heig-vd.ch>
2023-11-14 17:31:28 +0100

    update stack

--- starter/util/Stack.java
+++ starter/util/Stack.java
@@ -1,4 +1,33 @@
 package util;
 
-public class Stack {
+public class Stack<T> {
+    private class Element {
+        private final T value;
+        private final Element next;
+
+        public Element(T value, Element next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public void stack(T value) {
+    }
+
+    public void unstack() {
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public T[] getStack() {
+        return null;
+    }
+
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+
 }
